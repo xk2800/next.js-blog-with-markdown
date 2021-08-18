adc3e35b824dab67e76779bf1f7bb8c0f6c05ef1
Xavier K <xavierkhew00@gmail.com>
2021-08-18 18:03:17 +0800

    Get Slug & Frontmatter & displaying posts

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import fs from 'fs';
 import path from 'path';
+import matter from 'gray-matter';
 import Head from 'next/head';
 
+import Post from '../components/Post';
+
 export default function Home({ posts }) {
-    console.log(posts);
+
+    //console.log(posts);
+
     return (
         <div>
             <Head>
@@ -12,7 +17,12 @@ export default function Home({ posts }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <h2>Hello there</h2>
+            <div className="posts">
+                {posts.map((post, index) => (
+                    // <h3>{post.frontmatter.title}</h3>
+                    <Post post={post} />
+                ))}
+            </div>
         </div>
 
     );
@@ -21,13 +31,35 @@ export default function Home({ posts }) {
 //fetch data //run as server side 
 export async function getStaticProps() {
 
+    // Get files from posts dir
     const files = fs.readdirSync(path.join('posts'));
 
     console.log(files);
 
+    // Get slug n frontmatter from posts
+    const posts = files.map(filename => {
+        // Create slug
+        const slug = filename.replace('.md', '');
+
+        // Get frontmatter (content)
+        const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8');
+
+        //console.log(markdownWithMeta);
+
+        // Parsing frontmatter
+        const { data: frontmatter } = matter(markdownWithMeta);
+
+        return {
+            slug,
+            frontmatter,
+        };
+    });
+
+    //console.log(posts);
+
     return {
         props: {
-            posts: 'Postings'
-        }
+            posts,
+        },
     };
 }
